d2c9cea94c89de7f866cfee3452779be1dcf5920
Paul Tower <ptower@hypercubeapps.com>
2014-04-29 14:50:06 -0400

    Updated CardFlight SDK Added tokenization

--- SDKSandbox/CFTTestViewController.m
+++ SDKSandbox/CFTTestViewController.m
@@ -9,11 +9,12 @@
 #import "CFTTestViewController.h"
 #import "CFTReader.h"
 #import "CFTCard.h"
+#import "CFTCharge.h"
 #import "CardFlight.h"
 #import "CFTPaymentView.h"
 #import "SVProgressHUD.h"
 
-@interface CFTTestViewController () <CFTReaderDelegate, CFTPaymentViewDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate> {
+@interface CFTTestViewController () <CFTReaderDelegate, CFTPaymentViewDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate, UIAlertViewDelegate> {
     
     CGFloat animatedDistance;
 }
@@ -25,6 +26,8 @@
 @property (nonatomic) UILabel *readerStatus;
 @property (nonatomic) UILabel *messageLabel;
 @property (nonatomic) UITextField *timeoutText;
+@property (nonatomic) UIButton *swipeButton;
+@property (nonatomic) UIButton *tokenizeButton;
 @property (nonatomic) UIButton *chargeButton;
 @property (nonatomic) CFTPaymentView *paymentView;
 
@@ -45,10 +48,6 @@ static NSString *ACCOUNT_TOKEN = @"PUT_YOUR_ACCOUNT_TOKEN_HERE";
 
 
 static NSString *kDefaultFont = @"Avenir";
-static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3;
-static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2;
-static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8;
-static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
 
 @implementation CFTTestViewController
 
@@ -99,12 +98,13 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
                            [[UIDevice currentDevice] systemVersion]]];
     [versionLabel setFont:defaultFont];
     
-    UIButton *swipeButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [swipeButton setTitle:@"Attempt Swipe" forState:UIControlStateNormal];
-    [swipeButton.titleLabel setFont:defaultFont];
-    [swipeButton addTarget:self
-                    action:@selector(swipeCard:)
-          forControlEvents:UIControlEventTouchUpInside];
+    _swipeButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [_swipeButton setTitle:@"Attempt Swipe" forState:UIControlStateNormal];
+    [_swipeButton.titleLabel setFont:defaultFont];
+    [_swipeButton addTarget:self
+                     action:@selector(swipeCard:)
+           forControlEvents:UIControlEventTouchUpInside];
+    [_swipeButton setEnabled:NO];
     
     UIButton *serialButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [serialButton setTitle:@"Serial Number" forState:UIControlStateNormal];
@@ -150,6 +150,14 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     [_chargeButton addTarget:self
                       action:@selector(chargeButtonPressed:)
             forControlEvents:UIControlEventTouchUpInside];
+    [_chargeButton setEnabled:NO];
+    
+    _tokenizeButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [_tokenizeButton setTitle:@"Test Tokenize" forState:UIControlStateNormal];
+    [_tokenizeButton addTarget:self
+                        action:@selector(tokenizeButtonPressed:)
+              forControlEvents:UIControlEventTouchUpInside];
+    [_tokenizeButton setEnabled:NO];
     
     _readerStatus = [[UILabel alloc] init];
     [_readerStatus setTextAlignment:NSTextAlignmentCenter];
@@ -157,9 +165,14 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     [_readerStatus setText:@"NOT CONNECTED"];
     [_readerStatus setFont:defaultFont];
     
+    _paymentView = [[CFTPaymentView alloc] initWithFrame:CGRectZero];
+    [_paymentView setDelegate:self];
+    [_paymentView useFont:[UIFont fontWithName:kDefaultFont size:17]];
+    [_paymentView useKeyboardAppearance:UIKeyboardAppearanceDark];
+    
     [headerLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     [versionLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [swipeButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_swipeButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     [serialButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     [_nameLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     [_numberLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
@@ -168,10 +181,12 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     [timeoutButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     [_readerStatus setTranslatesAutoresizingMaskIntoConstraints:NO];
     [_chargeButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_tokenizeButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_paymentView setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     [self.view addSubview:headerLabel];
     [self.view addSubview:versionLabel];
-    [self.view addSubview:swipeButton];
+    [self.view addSubview:_swipeButton];
     [self.view addSubview:serialButton];
     [self.view addSubview:_nameLabel];
     [self.view addSubview:_numberLabel];
@@ -180,8 +195,10 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     [self.view addSubview:timeoutButton];
     [self.view addSubview:_readerStatus];
     [self.view addSubview:_chargeButton];
+    [self.view addSubview:_tokenizeButton];
+    [self.view addSubview:_paymentView];
     
-    NSDictionary *constraints = NSDictionaryOfVariableBindings(headerLabel, versionLabel, swipeButton, serialButton, timeoutButton, _readerStatus, _chargeButton, _messageLabel, _nameLabel, _numberLabel);
+    NSDictionary *constraints = NSDictionaryOfVariableBindings(headerLabel, versionLabel, _swipeButton, _paymentView, serialButton, timeoutButton, _readerStatus, _chargeButton, _tokenizeButton, _messageLabel, _nameLabel, _numberLabel);
     if ([self respondsToSelector:@selector(topLayoutGuide)]) {
         id topGuide = self.topLayoutGuide;
         [self addVisualConstraints:@"V:[topGuide]-[headerLabel]"
@@ -191,22 +208,25 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
                     withDictionary:NSDictionaryOfVariableBindings(headerLabel)];
     }
     
-    [self addVisualConstraints:@"V:[headerLabel]-12-[versionLabel]-15-[swipeButton]"
+    [self addVisualConstraints:@"V:[headerLabel]-12-[versionLabel]-15-[_swipeButton]-15-[_paymentView(45)]-15-[_nameLabel]-[_numberLabel]-[_messageLabel]"
                 withDictionary:constraints];
-    [self addVisualConstraints:@"V:[_nameLabel]-[_numberLabel]-[_messageLabel]-15-[_chargeButton]-15-[serialButton]-15-[timeoutButton]-20-[_readerStatus]-|"
+    [self addVisualConstraints:@"V:[_tokenizeButton]-[_chargeButton]-[serialButton]-[timeoutButton]-15-[_readerStatus]-|"
                 withDictionary:constraints];
     [self addVisualConstraints:@"|-[headerLabel]-|" withDictionary:NSDictionaryOfVariableBindings(headerLabel)];
     [self addVisualConstraints:@"|-[versionLabel]-|" withDictionary:NSDictionaryOfVariableBindings(versionLabel)];
-    [self addVisualConstraints:@"|-[swipeButton]-|" withDictionary:NSDictionaryOfVariableBindings(swipeButton)];
+    [self addVisualConstraints:@"|-[_swipeButton]-|" withDictionary:NSDictionaryOfVariableBindings(_swipeButton)];
+    [self addVisualConstraints:@"[_paymentView(290)]" withDictionary:NSDictionaryOfVariableBindings(_paymentView)];
     [self addVisualConstraints:@"|-[serialButton]-|" withDictionary:NSDictionaryOfVariableBindings(serialButton)];
     [self addVisualConstraints:@"|-[_nameLabel]-|" withDictionary:NSDictionaryOfVariableBindings(_nameLabel)];
     [self addVisualConstraints:@"|-[_numberLabel]-|" withDictionary:NSDictionaryOfVariableBindings(_numberLabel)];
     [self addVisualConstraints:@"|-[_messageLabel]-|" withDictionary:NSDictionaryOfVariableBindings(_messageLabel)];
     [self addVisualConstraints:@"|-[_chargeButton]-|" withDictionary:NSDictionaryOfVariableBindings(_chargeButton)];
+    [self addVisualConstraints:@"|-[_tokenizeButton]-|" withDictionary:NSDictionaryOfVariableBindings(_tokenizeButton)];
     [self addVisualConstraints:@"[_timeoutText(45)]-[timeoutButton]"
                 withDictionary:NSDictionaryOfVariableBindings(_timeoutText, timeoutButton)];
     [self addVisualConstraints:@"|-[_readerStatus]-|" withDictionary:NSDictionaryOfVariableBindings(_readerStatus)];
     
+    [self addEqualConstraint:NSLayoutAttributeCenterX from:_paymentView to:_paymentView.superview];
     [self addEqualConstraint:NSLayoutAttributeCenterY from:_timeoutText to:timeoutButton];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:timeoutButton
                                                           attribute:NSLayoutAttributeCenterX
@@ -215,12 +235,6 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
                                                           attribute:NSLayoutAttributeCenterX
                                                          multiplier:1
                                                            constant:22]];
-    
-    _paymentView = [[CFTPaymentView alloc] initWithFrame:CGRectMake(15, 150, 290, 45)];
-    [_paymentView setDelegate:self];
-    [_paymentView useFont:[UIFont fontWithName:kDefaultFont size:17]];
-    [_paymentView useKeyboardAppearance:UIKeyboardAppearanceDark];
-    [self.view addSubview:_paymentView];
 }
 
 #pragma mark - Private Methods
@@ -246,7 +260,35 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
 
 - (void)chargeButtonPressed:(id)sender {
     
-    [self chargeCard];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"CardFlight Demo"
+                                                    message:@"Enter charge amount"
+                                                   delegate:self
+                                          cancelButtonTitle:@"Cancel"
+                                          otherButtonTitles:@"Test Charge", nil];
+    [alert setAlertViewStyle:UIAlertViewStylePlainTextInput];
+    [[alert textFieldAtIndex:0] setDelegate:self];
+    [[alert textFieldAtIndex:0] setKeyboardType:UIKeyboardTypeNumberPad];
+    [[alert textFieldAtIndex:0] setKeyboardAppearance:UIKeyboardAppearanceDark];
+    [[alert textFieldAtIndex:0] setTextAlignment:NSTextAlignmentRight];
+    [alert show];
+//    [self chargeCard];
+}
+
+- (void)tokenizeButtonPressed:(id)sender {
+    
+    [SVProgressHUD showWithStatus:@"Tokenizing" maskType:SVProgressHUDMaskTypeClear];
+    if (_card) {
+        [_card tokenizeCardWithSuccess:^{
+            [SVProgressHUD showSuccessWithStatus:@"Success"];
+            [_messageLabel setText:[NSString stringWithFormat:@"Successfully tokenized: %@", _card.cardToken]];
+        }
+                               failure:^(NSError *error){
+                                   [SVProgressHUD showErrorWithStatus:@"Failed"];
+                                   [self displayError:error];
+                               }];
+    } else {
+        [_messageLabel setText:@"Error - No card to tokenize"];
+    }
 }
 
 - (void)dismissKeyboard {
@@ -260,6 +302,8 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     [_nameLabel setText:@""];
     [_numberLabel setText:@""];
     [_cardReader beginSwipeWithMessage:nil];
+    [_readerStatus setText:@"SWIPE NOW"];
+    [_swipeButton setEnabled:NO];
     [_messageLabel setText:@""];
 }
 
@@ -278,17 +322,19 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     [_cardReader swipeTimeoutDuration:[_timeoutText.text integerValue]];
 }
 
-- (void)chargeCard {
+- (void)chargeCardWithAmount:(NSDecimalNumber *)amount {
     
     if (_card) {
         [SVProgressHUD showWithStatus:@"Charging" maskType:SVProgressHUDMaskTypeClear];
-        NSDictionary *paymentInfo = @{@"amount":[NSDecimalNumber decimalNumberWithString:@"1.00"],
+        NSDictionary *paymentInfo = @{@"amount":amount,
                                       @"currency": @"USD",
                                       @"description": @"Description"};
         [_card chargeCardWithParameters:paymentInfo
                                 success:^(CFTCharge *charge) {
                                     [SVProgressHUD showSuccessWithStatus:@"Success"];
-                                    [_messageLabel setText:[NSString stringWithFormat:@"Successfully charged: %@", charge]];
+                                    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+                                    [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+                                    [_messageLabel setText:[NSString stringWithFormat:@"Successfully charged: %@", [formatter stringFromNumber:charge.amount]]];
                                 }
                                 failure:^(NSError *error) {
                                     [SVProgressHUD showErrorWithStatus:@"Failed"];
@@ -304,16 +350,22 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
 - (void)keyedCardResponse:(CFTCard *)card {
     
     _card = card;
+    [_chargeButton setEnabled:YES];
+    [_tokenizeButton setEnabled:YES];
 }
 
 #pragma mark - Reader Delegate
 
 - (void)readerCardResponse:(CFTCard *)card withError:(NSError *)error {
     
+    [_swipeButton setEnabled:YES];
+    [_readerStatus setText:@"CONNECTED"];
     if (!error) {
         [_nameLabel setText:card.name];
         [_numberLabel setText:card.encryptedCardNumber];
         _card = card;
+        [_chargeButton setEnabled:YES];
+        [_tokenizeButton setEnabled:YES];
     }
     else {
         [self displayError:error];
@@ -324,17 +376,18 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     
     if (isConnected) {
         [_readerStatus setText:@"CONNECTED"];
-        [_readerStatus setTextColor:[UIColor greenColor]];
         [self swipeCard:nil];
     }
     else {
-        // NSLog(@"Error: %@", error);
+        [_readerStatus setText:@"NOT CONNECTED"];
+        [_readerStatus setTextColor:[UIColor redColor]];
         [self displayError:error];
     }
 }
 
 - (void)readerIsAttached {
     
+    [_messageLabel setText:@""];
     [_readerStatus setText:@"CONNECTING"];
     [_readerStatus setTextColor:[UIColor blueColor]];
 }
@@ -343,6 +396,7 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     
     [_readerStatus setText:@"NOT CONNECTED"];
     [_readerStatus setTextColor:[UIColor redColor]];
+    [_swipeButton setEnabled:NO];
 }
 
 - (void)readerSerialNumber:(NSString *)serialNumber {
@@ -350,63 +404,37 @@ static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
     [_messageLabel setText:[NSString stringWithFormat:@"Serial Number: %@", serialNumber]];
 }
 
-#pragma mark - UITextField Delegate
+#pragma mark - Alert View Delegate
 
-- (void)textFieldDidBeginEditing:(UITextField *)textField {
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     
-    CGRect textFieldRect = [self.view.window convertRect:textField.bounds fromView:textField];
-    CGRect viewRect = [self.view.window convertRect:self.view.bounds fromView:self.view];
-    CGFloat midline = textFieldRect.origin.y + 0.5 * textFieldRect.size.height;
-    CGFloat numerator = midline - viewRect.origin.y - MINIMUM_SCROLL_FRACTION * viewRect.size.height;
-    CGFloat denominator = (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION) * viewRect.size.height;
-    CGFloat heightFraction = numerator / denominator;
-    
-    if (heightFraction < 0.0) {
-        heightFraction = 0.0;
+    if (buttonIndex == 1) {
+        NSString *cleanCentString = [[[alertView textFieldAtIndex:0].text componentsSeparatedByCharactersInSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]] componentsJoinedByString:@""];
+        NSDecimalNumber *amount = [[NSDecimalNumber decimalNumberWithString:cleanCentString] decimalNumberByMultiplyingByPowerOf10:-2];
+        [self chargeCardWithAmount:amount];
     }
-    else if (heightFraction > 1.0) {
-        heightFraction = 1.0;
-    }
+}
+
+#pragma mark - Text Field Delegate
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     
-    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+    NSString *cleanCentString = [[textField.text componentsSeparatedByCharactersInSet: [[NSCharacterSet decimalDigitCharacterSet] invertedSet]] componentsJoinedByString:@""];
+    NSInteger centValue= cleanCentString.integerValue;
     
-    if (orientation == UIInterfaceOrientationPortrait || orientation == UIInterfaceOrientationPortraitUpsideDown) {
-        animatedDistance = floor(PORTRAIT_KEYBOARD_HEIGHT * heightFraction);
+    if (string.length > 0) {
+        centValue = centValue * 10 + string.integerValue;
     }
     else {
-        //animatedDistance = floor(LANDSCAPE_KEYBOARD_HEIGHT * heightFraction);
+        centValue = centValue / 10;
     }
     
-    CGRect viewFrame = self.view.frame;
-    viewFrame.origin.y -= animatedDistance;
-    
-    [UIView animateWithDuration:KEYBOARD_ANIMATION_DURATION
-                          delay:0
-                        options:UIViewAnimationOptionBeginFromCurrentState
-                     animations:^{
-                         [self.view setFrame:viewFrame];
-                     }
-                     completion:nil];
-}
-
-- (void)textFieldDidEndEditing:(UITextField *)textField {
-    
-    CGRect viewFrame = self.view.frame;
-    viewFrame.origin.y += animatedDistance;
-    
-    [UIView animateWithDuration:KEYBOARD_ANIMATION_DURATION
-                          delay:0
-                        options:UIViewAnimationOptionBeginFromCurrentState
-                     animations:^{
-                         [self.view setFrame:viewFrame];
-                     }
-                     completion:nil];
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    
-    [textField resignFirstResponder];
-    return YES;
+    NSNumber *formatedValue;
+    formatedValue = [[NSNumber alloc] initWithFloat:(float)centValue / 100.0f];
+    NSNumberFormatter *_currencyFormatter = [[NSNumberFormatter alloc] init];
+    [_currencyFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    textField.text = [_currencyFormatter stringFromNumber:formatedValue];
+    return NO;
 }
 
 @end
